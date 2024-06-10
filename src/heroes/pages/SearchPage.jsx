167b7aa12f63164485b09813f1287650c560f89c
Oliver31Batista <oliver31batista@gmail.com>
2024-06-10 14:44:54 -0400

    Designing the search screen

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,5 +1,35 @@
+import { HeroCard } from "../components";
+
 export const SearchPage = () => {
   return (
-    <h1>SearchPage</h1>
-  )
-}
+    <>
+      <div className="row">
+        <div className="col-5">
+          <h1>Search Page</h1>
+          <hr />
+          <form action="">
+            <input
+              type="text"
+              placeholder="Search a hero"
+              className="form-control"
+              name="searchText"
+              autoComplete="off"
+            />
+            <div className="btn btn-outline-dark mt-1">🔍</div>
+          </form>
+        </div>
+
+        <div className="col-7">
+          <h4>Results</h4>
+          <hr />
+
+          <div className="alert alert-dark">Search a Hero</div>
+
+          <div className="alert alert-danger">No hero with <b>abc...</b></div>
+
+          {/* <HeroCard /> */}
+        </div>
+      </div>
+    </>
+  );
+};
