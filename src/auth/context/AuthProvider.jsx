e79dfaaa632fc1a9e7e65fcdf005f7914f2ea050
Oliver31Batista <oliver31batista@gmail.com>
2024-06-11 16:16:27 -0400

    Maintaining the user's login status

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -3,21 +3,27 @@ import { AuthContext } from "./AuthContext";
 import { authReducer } from "./authReducer";
 import { types } from "../types";
 
-const initialState = {
-  logged: false,
+const init = () => {
+  const user = JSON.parse(localStorage.getItem("user"));
+
+  return {
+    logged: !!user,
+    user: user,
+  };
 };
 
 export const AuthProvider = ({ children }) => {
-  const [authState, authDispatch] = useReducer(authReducer, initialState);
+  const [authState, authDispatch] = useReducer(authReducer, {}, init);
 
   const login = (name = "") => {
+    const user = { id: "ABC", name };
+
     const action = {
       type: types.login,
-      payload: {
-        id: "abc",
-        name: name,
-      },
+      payload: user,
     };
+
+    localStorage.setItem("user", JSON.stringify(user));
 
     authDispatch(action);
   };
