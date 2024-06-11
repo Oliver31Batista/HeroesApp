580aafe279f4da705e76847f6f07b0d350325543
Oliver31Batista <oliver31batista@gmail.com>
2024-06-11 17:11:19 -0400

    I added private routes

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,13 +2,23 @@ import { Routes, Route } from "react-router-dom";
 
 import { HeroesRoutes } from "../heroes";
 import { LoginPage } from "../auth";
+import { PrivateRoute } from "./PrivateRoute";
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
         <Route path="login" element={<LoginPage />} />
-        <Route path="/*" element={<HeroesRoutes to="/marvel" />} />
+        <Route
+          path="/*"
+          element={
+            <PrivateRoute>
+              <Routes>
+                <Route path="/*" element={<HeroesRoutes to="/marvel" />} />
+              </Routes>
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </>
   );
